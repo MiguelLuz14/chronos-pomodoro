e17116e72ac596a372528720a4609a881a59cece
Miguel Luz <migoluz@gmail.com>
2026-03-25 04:16:30 -0300

    colocando css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <h1>Ola mundo</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, a?
-        Excepturi incidunt voluptate, eaque quasi doloremque distinctio vitae
-        laudantium modi nisi sapiente asperiores consectetur odio rem
-        voluptatibus molestiae voluptates et.
-      </p>
+      <Heading />
     </>
   );
 }
