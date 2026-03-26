26877aba9168ca03eb786103348854bd680799e7
Miguel Luz <migoluz@gmail.com>
2026-03-26 16:02:39 -0300

    atualizando as mudancas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>
-      <Heading />
+      <Heading>
+        Ola mundo
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A pariatur
+        harum inventore quasi natus hic laborum debitis quos provident possimus,
+        labore in nemo! At optio voluptatem possimus aspernatur id quo!
+      </p>
     </>
   );
 }
