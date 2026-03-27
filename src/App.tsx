758f3231b67d8728394669c1a09a71081038d382
Miguel Luz <migoluz@gmail.com>
2026-03-27 01:09:29 -0300

    realizando o molde do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A pariatur
-        harum inventore quasi natus hic laborum debitis quos provident possimus,
-        labore in nemo! At optio voluptatem possimus aspernatur id quo!
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+
+      <Container>
+        <Heading>FORMULARIO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>FOOTER</Heading>
+      </Container>
     </>
   );
 }
