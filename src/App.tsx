1963fabbe44b0c2737c71a5abdea54aa8feaf7cd
Miguel Luz <migoluz@gmail.com>
2026-03-31 16:20:58 -0300

    Adicionei os icones no menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
+export { Timer } from 'lucide-react';
+export { Clock } from 'lucide-react';
+export { Play } from 'lucide-react';
+export { Pause } from 'lucide-react';
+export { Square } from 'lucide-react';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
 
       <Container>
